7a0e5a7504c9175516e713cab0a3da9d3c321a3a
lifeinbofyx@sina.com <lifeinbofyx@sina.com>
2017-01-31 01:57:55 +0800

    common update 01311
    update code for loops.

--- routes/todos.js
+++ routes/todos.js
@@ -34,6 +34,7 @@ router.post('/', function(req, res, next) {
   var content = req.body.content;
   var todo = new Todo();
   var sites = ['www.baidu.com/s?ie=utf-8&wd=','http://weixin.sogou.com/weixin?type=2&query='];
+  var site = '';
   for (site in sites){
   	todo.set('content', sites+content);
   }
